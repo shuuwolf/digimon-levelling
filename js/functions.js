be73c7abcbbd26c7b325ac5dc8188dda8b1f9930
shuuwolf <64663811+shuuwolf@users.noreply.github.com>
2021-02-11 17:04:48 -0300

    s
    s

---
+++ js/functions.js
@@ -0,0 +1,94 @@
+$(function() {
+
+    var level = document.getElementById("LEVEL");
+    var HP = document.getElementById("HP");
+    var ATK = document.getElementById("ATK");
+    var DEF = document.getElementById("DEF");
+    var CRIT = document.getElementById("CRIT");
+
+    $( "#levelup" ).click(function() {
+        alert( "Level UP !" );
+        $('#1').remove();
+        $('#2').removeClass('off');
+          $( "#levelup" ).click(function() {
+              $('#2').remove();
+              $('#3').removeClass('off');
+                $( "#levelup" ).click(function() {
+                  $('#3').remove();
+                  $('#4').removeClass('off');
+                    $( "#levelup" ).click(function() {
+                      $('#4').remove();
+                      $('#5').removeClass('off');
+                      $('#img_1').remove();
+                      $('#img_2').removeClass('off');
+                        $( "#levelup" ).click(function() {
+                          $('#5').remove();
+                          $('#6').removeClass('off');
+                            $( "#levelup" ).click(function() {
+                              $('#6').remove();
+                              $('#7').removeClass('off');
+                                $( "#levelup" ).click(function() {
+                                  $('#7').remove();
+                                  $('#8').removeClass('off');
+                                    $( "#levelup" ).click(function() {
+                                      $('#8').remove();
+                                      $('#9').removeClass('off');
+                                        $( "#levelup" ).click(function() {
+                                          $('#9').remove();
+                                          $('#10').removeClass('off');
+                                          $('#img_2').remove();
+                                          $('#img_3').removeClass('off');
+                                            $( "#levelup" ).click(function() {
+                                              $('#10').remove();
+                                              $('#11').removeClass('off');
+                                                $( "#levelup" ).click(function() {
+                                                  $('#11').remove();
+                                                  $('#12').removeClass('off');
+                                                    $( "#levelup" ).click(function() {
+                                                      $('#12').remove();
+                                                      $('#13').removeClass('off');
+                                                        $( "#levelup" ).click(function() {
+                                                          $('#13').remove();
+                                                          $('#14').removeClass('off');
+                                                            $( "#levelup" ).click(function() {
+                                                              $('#14').remove();
+                                                              $('#15').removeClass('off');
+                                                                $( "#levelup" ).click(function() {
+                                                                  $('#15').remove();
+                                                                  $('#16').removeClass('off');
+                                                                  $('#img_3').remove();
+                                                                  $('#img_4').removeClass('off');
+                                                                    $( "#levelup" ).click(function() {
+                                                                      $('#16').remove();
+                                                                      $('#17').removeClass('off');
+                                                                      $( "#levelup" ).click(function() {
+                                                                        $('#17').remove();
+                                                                        $('#18').removeClass('off');
+                                                                          $( "#levelup" ).click(function() {
+                                                                            $('#18').remove();
+                                                                            $('#19').removeClass('off');
+                                                                              $( "#levelup" ).click(function() {
+                                                                                $('#19').remove();
+                                                                                $('#20').removeClass('off');
+                                                                                $('#img_4').remove();
+                                                                                $('#img_5').removeClass('off');               
+                                                                        });                
+                                                                      });                
+                                                                    });                
+                                                                }); 
+                                                            });
+                                                          });
+                                          });
+                                        });
+                                    });
+                                  });
+                                });
+                            });
+                          });
+                        });
+                        });
+                    });
+              });
+          });
+      });
+});
